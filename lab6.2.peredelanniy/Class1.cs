86610331a3d60456451dfae2e0902c822f29dcb8
ssmitee <144239782+ssmitee@users.noreply.github.com>
2023-12-28 22:47:10 +0200

    Добавьте файлы проекта.

---
+++ lab6.2.peredelanniy/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6._2.peredelanniy
+{
+    internal class Class1
+    {
+    }
+}
+namespace MatrixLibrary
+{
+    public class MatrixOperations
+    {
+        public static double CalculateSumOfMinMax(double[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            double min = matrix[0, 0];
+            double max = matrix[0, 0];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] < min)
+                        min = matrix[i, j];
+
+                    if (matrix[i, j] > max)
+                        max = matrix[i, j];
+                }
+            }
+            double sum = min + max;
+
+            return sum;
+        }
+    }
+}
---
+++ lab6.2.peredelanniy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MatrixLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество строк: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите количество столбцов: ");
+        int columns = int.Parse(Console.ReadLine());
+
+        double[,] matrix = new double[rows, columns];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"Введите элемент [{i}, {j}]: ");
+                matrix[i, j] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        double sum = MatrixOperations.CalculateSumOfMinMax(matrix);
+
+        Console.WriteLine("Сумма максимального и минимального элементов матрицы: " + sum);
+    }
+
+
+}
